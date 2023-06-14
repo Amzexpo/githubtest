23bc01c60404d016896eed2c5a7c291fe17d10bf
Amzexpo <a2513953980@gmail.com>
2023-06-14 14:07:41 +0800

    第一次版本1提交

---
+++ BoostServer/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include "asio.h"
+
+
+
+int main() {
+    try {
+        boost::asio::io_context ioc;
+        Server server(ioc, 6000);
+        ioc.run();
+    }catch(std::exception &e) {
+        std::cout << "server start error " << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
