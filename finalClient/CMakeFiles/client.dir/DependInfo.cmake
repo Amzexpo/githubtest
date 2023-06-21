c174060103cf7fb7a862df3ddc36a3edab8625a6
Amzexpo <a2513953980@gmail.com>
2023-06-21 16:11:02 +0800

    于最终版本服务器对应的客户端

---
+++ finalClient/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubunturos/boostwork/testclient/client.cpp" "/home/ubunturos/boostwork/testclient/CMakeFiles/client.dir/client.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include:home/ubunturos/boost_1_82_0:home/ubunturos/mpich-3.2/bin:/opt/ros/noetic/bin:/home/ubunturos/.vscode-server/bin/695af097c7bd098fbf017ce3ac85e09bbc5dda06/bin/remote-cli:home/ubunturos/boost_1_82_0:home/ubunturos/mpich-3.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
